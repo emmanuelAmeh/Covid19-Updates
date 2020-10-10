73cc5bf139300e1e6e1d5774f154d17e0c175132
EmmanuelAmeh <immanuel.ameh@gmail.com>
2020-10-10 16:31:36 +0100

    added splash screen with sample logo Add splashscreen dependency

---
+++ app/src/main/java/com/example/android/covid19updates/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.example.android.covid19updates;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import gr.net.maroulis.library.EasySplashScreen;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EasySplashScreen configSplash = new EasySplashScreen(SplashScreenActivity.this)
+                .withFullScreen()
+                .withTargetActivity(MainActivity.class)
+                .withSplashTimeOut(4000)
+                .withBackgroundColor(Color.WHITE)
+                .withAfterLogoText(getString(R.string.splashscreen_text))
+                .withLogo(R.drawable.ic_local_hospital);
+
+        View easySplashScreen = configSplash.create();
+        setContentView(easySplashScreen);
+    }
+}
