38e798f3f2c61ec68e40f3b0185410deb0bd5d56
Gbolahan Awoseyila <iamgbolahan@yahoo.com>
2020-10-16 13:16:30 +0100

    Populated WorldActivity layout with data

---
+++ app/src/main/java/com/example/android/covid19updates/APIClient.java
@@ -0,0 +1,29 @@
+package com.example.android.covid19updates;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+class APIClient {
+    private static Retrofit retrofit = null;
+    private static Gson gson = new GsonBuilder().setLenient().create();
+    private static final String BASE_URL = "https://api.covid19api.com";
+
+    static Retrofit getClient() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(client)
+                .build();
+
+        return retrofit;
+    }
+}
