5d33209fc8de1cef16a84f668f17e7708362b10d
Joshua Eselebor <eseleborehizele@gmail.com>
2020-10-25 03:51:18 +0100

    added app icon and splashscreen

---
+++ app/src/main/java/com/example/android/covid19updates/Launch.java
@@ -0,0 +1,14 @@
+package com.example.android.covid19updates;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Launch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+    }
+}
